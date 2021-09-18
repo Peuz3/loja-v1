70a308bc8a1217c4810a9bb648d3df2f517035e8
Joao Pedro <jp.ssilva@live.com>
2021-09-18 10:49:55 -0300

    Replace the old site and all functionalities

--- config.php
+++ config.php
@@ -6,18 +6,20 @@ global $db;
 
 $config = array();
 if(ENVIRONMENT == 'development') {
-	define("BASE_URL", "http://localhost/loja-v1/");
+	define("BASE_URL", "http://localhost/loja_v1/");
 	$config['dbname'] = 'loja';
 	$config['host'] = 'localhost';
 	$config['dbuser'] = 'root';
 	$config['dbpass'] = '';
 } else {
-	define("BASE_URL", "http://localhost/loja-v1/");
+	define("BASE_URL", "http://localhost/loja_v1/");
 	$config['dbname'] = 'loja';
 	$config['host'] = 'localhost';
 	$config['dbuser'] = 'root';
 	$config['dbpass'] = '';
 }
+
+$config['default_lang'] = 'pt-br';
 
 $db = new PDO("mysql:dbname=".$config['dbname'].";host=".$config['host'], $config['dbuser'], $config['dbpass']);
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
